1000a88f8ac77d72d1e5a2d80b64cad0f35a636f
Danny Ocean <danny.danh.nguyen@gmail.com>
2019-06-05 13:30:56 -0700

    Single post display

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-// import Spinner from '../layout/Spinner';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [getPost()]);
+  }, [getPost]);
 
-  return <div>Post</div>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
